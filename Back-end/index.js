570cb02a494c55179fe952a23e21fa5d47afaaf8
wssfernando99 <sarindusauranga@gmail.com>
2024-01-26 23:57:24 +0530

    creating connection  with mongoDB

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app =  express();
 
-app.listen(PORT,()=> {
-    console.log(`listening: ${PORT}`)
-});
+app.get('/',(req, res) =>{
+    console.log(req)
+    return res.status(234).send('welcome to Task handling')
+})
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log('Database Connected');
+        app.listen(PORT,()=> {
+            console.log(`listening: ${PORT}`)
+        });
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+
+
