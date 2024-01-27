917414b8825a6024aa27afda40aa43ff9c99631e
wssfernando99 <sarindusauranga@gmail.com>
2024-01-27 20:25:28 +0530

    edit task

--- front-end/src/pages/Edittask.js
+++ front-end/src/pages/Edittask.js
@@ -1,8 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Edittask = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [duedate, setDuedate] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`http://localhost:5555/books/${id}`)
+    .then((response) => {
+        setDescription(response.data.description);
+        setDuedate(response.data.duedate)
+        setTitle(response.data.title)
+        setLoading(false);
+      }).catch((error) => {
+        setLoading(false);
+        alert('An error happened. Please Chack console');
+        console.log(error);
+      });
+  }, [])
+  
+  const handleEditBook = () => {
+    const data = {
+      title,
+      description,
+      duedate,
+    };
+    setLoading(true);
+    axios
+      .put(`http://localhost:5555/tasks/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error happened. Please Chack console');
+        console.log(error);
+      });
+  };
+
   return (
-    <div>Edittask</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Edit Task</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <input
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Description</label>
+          <input
+            type='text'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Due Date</label>
+          <input
+            type='number'
+            value={duedate}
+            onChange={(e) => setDuedate(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>
+          Save
+        </button>
+      </div>
+    </div>
   )
 }
 
